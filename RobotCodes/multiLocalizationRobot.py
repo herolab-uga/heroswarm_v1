a600c420297dbcc8b99c02df4a8b7333e019f62e
Michael Starks <starksmichael224@gmail.com>
2021-06-23 13:28:06 -0400

    Debugging

--- RobotCodes/multiLocalizationRobot.py
+++ RobotCodes/multiLocalizationRobot.py
@@ -120,7 +120,7 @@ def getTheta(pt11,pt12) -> float:
     vec1.append(pt12[0].astype(float)-pt11[0].astype(float))
     vec1.append(pt12[1].astype(float)-pt11[1].astype(float))
 
-    vec12dt=np.arctan2(vec1[1],vec1[0])
+    vec12dt=np.abs(np.arctan2(vec1[1],vec1[0]))
     print("Theta: " + str(vec12dt))
     return vec12dt
 
