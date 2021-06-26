fa018e86c339b59164bee3f46161cdbeebce4349
Michael Starks <starksmichael224@gmail.com>
2021-06-26 15:10:46 -0400

    debugging

--- RobotCodes/multiLocalizationRobot.py
+++ RobotCodes/multiLocalizationRobot.py
@@ -132,7 +132,7 @@ def MovOnTheta(theta):
                 if theta < 90 and theta > 0:
                     robot.turn_right(PID.get_speed(PID_out))
                 else:
-                    robot.turn_right(5)
+                    robot.turn_right(3)
             else:
                 # print('Go Straight')
                 robot.forward()
