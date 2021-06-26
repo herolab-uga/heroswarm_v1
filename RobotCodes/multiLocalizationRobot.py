63461b77e94936b9fb43f1486869cc47818faeb2
Michael Starks <starksmichael224@gmail.com>
2021-06-26 15:36:13 -0400

    debugging

--- RobotCodes/multiLocalizationRobot.py
+++ RobotCodes/multiLocalizationRobot.py
@@ -137,7 +137,7 @@ def MovOnTheta(theta):
                     robot.turn_right(0)
             else:
                 # print('Go Straight')
-                robot.backward()
+                robot.forward()
             # prev_theta = theta
         except Exception as e:
             print(e)
