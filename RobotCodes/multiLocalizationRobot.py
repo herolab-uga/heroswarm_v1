74af4f34c8dde46329c31df854005b503b6d2445
Michael Starks <starksmichael224@gmail.com>
2021-06-26 11:08:27 -0400

    debugging

--- RobotCodes/multiLocalizationRobot.py
+++ RobotCodes/multiLocalizationRobot.py
@@ -126,15 +126,15 @@ def MovOnTheta(theta):
             error = np.insert(error[:-1],0,delta_theta)
             # print(error)
             angle = PID.get_angle(error)
-            # print("PID: " + str(angle)) 
-            # if np.abs(theta) > thetaMargin:
-            #     if theta < 90 and theta > 0:
-            #         robot.turn_right(PID.get_speed(angle))
-            #     else:
-            #         robot.turn_right(5)
-            # else:
-            #     # print('Go Straight')
-            #     robot.forward()
+            print("PID: " + str(angle)) 
+            if np.abs(theta) > thetaMargin:
+                if theta < 90 and theta > 0:
+                    robot.turn_right(PID.get_speed(angle))
+                else:
+                    robot.turn_right(5)
+            else:
+                # print('Go Straight')
+                robot.forward()
             prev_theta = theta
         except Exception as e:
             print(e)
