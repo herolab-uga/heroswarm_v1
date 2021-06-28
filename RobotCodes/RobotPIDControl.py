ee791b829af804977f7be212019bdb7e67b55646
sanjayovs <sanjayovs@gmail.com>
2021-06-28 19:52:05 -0400

    Debug

--- RobotCodes/RobotPIDControl.py
+++ RobotCodes/RobotPIDControl.py
@@ -11,8 +11,6 @@ from SwarmRobot import SwarmRobot
 class PIDController:
     def __init__(self,parameters,setPoint):
     # This PID is for alignment with the endpoint
-        init = 10 # Integral window
-        diff = 5  # Differentiator
         self.p_k=parameters
         self.setPoint=setPoint
         # p_k = parameters['p']
@@ -21,35 +19,34 @@ class PIDController:
         # max_speed = 15
 
         # Computes the PID value usind the errors array
-        def get_correction(theta):
-            thetaError=setPoint-theta
-            pidCorrection=PIDCorrection(thetaError)
-            if(pidCorrection <0):
-                return 'turnRight'
-            elif(pidCorrection >0):
-                return 'turnLeft'
+    def get_correction(self,theta):
+        thetaError=self.setPoint-theta
+        pidCorrection=self.PIDCorrection(thetaError)
+        if(pidCorrection <0):
+            return 'turnRight'
+        elif(pidCorrection >0):
+            return 'turnLeft'
             
 
 
             
-        def PIDCorrection(thetaError):    
-
-            p_controller=self.p_k*thetaError
-            return p_controller*-1
-
+    def PIDCorrection(self,thetaError):    
+        p_controller=self.p_k*thetaError
+        return p_controller*-1
 
 
-        def get_angle(errors):
-            p_control = PID.p_k * errors[0]
-            i_control = PID.i_k * np.sum(errors)
-            # d = PID.d_k * np.sum( -1 * np.diff(errors, n=PID.diff))
-            d_control = 0
-            correctionSignal=p_control+i_control+d_control
-            return correctionSignal
 
-        # Normalizes the PID value and uses to detirmine angular speed
+    # def get_angle(errors):
+    #     p_control = PID.p_k * errors[0]
+    #     i_control = PID.i_k * np.sum(errors)
+    #     # d = PID.d_k * np.sum( -1 * np.diff(errors, n=PID.diff))
+    #     d_control = 0
+    #     correctionSignal=p_control+i_control+d_control
+    #     return correctionSignal
 
-        def get_speed(PID_out):
-            if np.abs(PID_out) > 180:
-                PID_out = 180
-            return np.abs(PID_out / 180) * PID.max_speed
+    # # Normalizes the PID value and uses to detirmine angular speed
+
+    # def get_speed(PID_out):
+    #     if np.abs(PID_out) > 180:
+    #         PID_out = 180
+    #     return np.abs(PID_out / 180) * PID.max_speed
