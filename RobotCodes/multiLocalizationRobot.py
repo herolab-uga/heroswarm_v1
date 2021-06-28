b8ad51820ddd4ae9d514fc1d2e1ebbb92279e1a8
sanjayovs <sanjayovs@gmail.com>
2021-06-28 19:11:19 -0400

    Debug

--- RobotCodes/multiLocalizationRobot.py
+++ RobotCodes/multiLocalizationRobot.py
@@ -111,12 +111,13 @@ def MovOnTheta(theta, distance):
     eStatus = True
     # Scales the margin angle with distance from the target (farther away less margin for error)
     #thetaMargin = 22.5 - distance / 232 * 22.5  # 116 - max distance possible between tag and robot
-    thetaMargin=20
+    thetaMargin=30
     thetaMargin1=thetaMargin/2
     thetaMargin2=-thetaMargin/2
     # error = -theta
     if not stpFlag and eStatus:
         try:
+            print (theta)
             # If the robot heading is outside the target threshold (thetaMargin) turn the robot
             if theta > thetaMargin1:
                 robot.turn_right(46)
