e7299137d0bf7925ffe1d7a55665144729963041
sanjayovs <sanjayovs@gmail.com>
2021-06-28 20:32:53 -0400

    Debug

--- RobotCodes/multiLocalizationRobot.py
+++ RobotCodes/multiLocalizationRobot.py
@@ -154,10 +154,10 @@ def getThetaDistance(startpoint, endpoint, heading):
     dif_dist=float(np.sqrt(float(rob_end_vec[0]) ** 2 + float(rob_end_vec[1]) ** 2))
     # If not a special case angle_end is described below
     angle_end = np.arctan2(rob_end_vec[1],rob_end_vec[0])
-    # print(angle_end)
+    print(angle_end)
     # Try to get the angle of the robot
-    angle_robot = np.arctan2(heading_rob[1],heading_rob[0])
-    print(np.degrees(angle_robot))
+    angle_robot = np.arctan2(heading_rob[1],heading_rob[0])  # This has been verfied
+    # print(np.degrees(angle_robot))
     # Calculates the rl_angle for the robot
     if angle_end > angle_robot:
         rl_angle = angle_end - angle_robot
