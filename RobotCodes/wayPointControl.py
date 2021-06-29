467948f2f955a29f2f18e392424e784f3ddb94aa
sanjayovs <sanjayovs@gmail.com>
2021-06-29 17:14:03 -0400

    Debug

--- RobotCodes/wayPointControl.py
+++ RobotCodes/wayPointControl.py
@@ -29,7 +29,7 @@ left = data["left"]
 
 # wayPoints=[(93,139),(95,109),(91,76),(92,36),(88,13),(80,24)]
 wayPoints=[(92,36),(88,13),(80,24)]
-wayPoint_delay=[0,0,0]
+wayPoint_delays=[0,0,0]
 # wayPoint_delays=[0,0,0,0,0,0]
 
 # Array that stores error values
