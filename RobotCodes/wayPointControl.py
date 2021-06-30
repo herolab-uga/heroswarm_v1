d4d3f93ff074e4096380746b233863cbcd9e5386
Michael Starks <starksmichael224@gmail.com>
2021-06-30 00:08:31 -0400

    coords

--- RobotCodes/wayPointControl.py
+++ RobotCodes/wayPointControl.py
@@ -26,13 +26,12 @@ with open('config/swarm_v1_config.JSON', 'r') as file:
 # Gets the right and lift duty cycles from json
 right = data["right"]
 left = data["left"]
-wayPoints=[(76,120)]
-wayPoints=[(77,137),(73,117),(69,103),(65,92)]                 #-90             # Upt 1
-wayPoints=[(62,152),(58,137),(56,117),(56,87),(59,70),(64,58)] #-45             # Upt 2
-wayPoints=[(87,14),(77,19),(70,30),(69,42)]                    #0               # Upt 3
-wayPoints=[(89,35),(82,37),(78,40)]                            #180             # Upt 4
-wayPoints=[(88,56),(81,59)]                                    #-135            # Upt 5
-wayPoints=[(86,137),(84,115),(83,91)]                          #-90             # Upt 6
+wayPoints=[(77,55),(73,47),(67,41),(64,37)]                 #-90             # Upt 1
+wayPoints=[(51,61),(57,54),(55,46),(55,35),(58,28),(63,24)] #-45             # Upt 2
+wayPoints=[(85,6),(75,8),(69,12),(68,17)]                    #0               # Upt 3
+wayPoints=[(87,14),(87,16),(77,16)]                            #180             # Upt 4
+wayPoints=[(85,23),(79,24)]                                    #-135            # Upt 5
+wayPoints=[(84,55),(82,46),(81,36)]                          #-90             # Upt 6
 
 
 # wayPoints=[(93,139),(95,109),(91,76),(92,36),(91,20),(80,24)]
