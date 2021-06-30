20c6a66e0e0e5706ffdcc824f30c649ea53f9ba1
sanjayovs <sanjayovs@gmail.com>
2021-06-30 13:35:18 -0400

    Debug

--- RobotCodes/rendzControl.py
+++ RobotCodes/rendzControl.py
@@ -142,7 +142,7 @@ def EndPointRend(allOdoData):
     return(meanX,meanY)
 
 def checkEndPointDist(robotX,robotY,endPtX,endPtY):
-    distanceThreshold=2
+    distanceThreshold=5
     if endPtX!=None and endPtY!=None and robotX!=None and robotY!=None:
         distanceReal=math.sqrt((robotX-endPtX)**2+(robotY-endPtY)**2)
         print('Distance:'+str(distanceReal))
